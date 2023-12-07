b30c803b840f521738916d5fea2308d109473bcb
cruzjeff225 <cruzjeff225>
2023-12-06 22:38:28 -0600

    Alojar tareas ingresadas

--- src/lib.rs
+++ src/lib.rs
@@ -1,24 +1,62 @@
-pub struct ToDo {
-    tasks: Vec<String>,
+use std::fmt;
+
+pub struct TodoList {
+    tasks: Vec<Task>,
 }
 
-impl ToDo {
-    pub fn new()-> ToDo {
-        ToDo { tasks: Vec::new() }
+pub struct Task {
+    description: String,
+    due_time: Option<String>,
+    completed: bool,
+}
+
+impl Task {
+    pub fn new(description: String, due_time: Option<String>) -> Self {
+        Task {
+            description,
+            due_time,
+            completed: false,
+        }
     }
 
-    pub fn new_task(&mut self, task: String) {
+    pub fn complete(&mut self) {
+        self.completed = true;
+    }
+}
+
+impl fmt::Display for Task {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "{} - {}",
+            if self.completed { "[✔]" } else { "[ ]" },
+            self.description
+        )?;
+        if let Some(due_time) = &self.due_time {
+            write!(f, " - Hora Limite: {}", due_time)?;
+        }
+        Ok(())
+    }
+}
+
+impl TodoList {
+    pub fn new() -> Self {
+        TodoList { tasks: Vec::new() }
+    }
+
+    pub fn add_task(&mut self, description: String, due_time: Option<String>) {
+        let task = Task::new(description, due_time);
         self.tasks.push(task);
     }
 
-    pub fn show_tasks(&self) {
+    pub fn display_tasks(&self) {
         if self.tasks.is_empty() {
             println!("No hay tareas pendientes.");
         } else {
             println!("Tareas pendientes:");
-            self.tasks.iter().enumerate().for_each(|(index, task)| {
+            for (index, task) in self.tasks.iter().enumerate() {
                 println!("{}. {}", index + 1, task);
-            });
+            }
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::io::{self, Write};
-use To_Do_List::ToDo;
+use To_Do_List::{TodoList, Task};
 
 fn main() {
-    let mut todo_list = ToDo::new();
+    let mut todo_list = TodoList::new();
 
     loop {
         print_menu();
@@ -15,22 +15,44 @@ fn main() {
                 println!("Ingrese la tarea:");
                 let mut task_input = String::new();
                 io::stdin().read_line(&mut task_input).expect("Error al leer la tarea");
-                todo_list.new_task(task_input.trim().to_string());
+
+                println!("¿TieneS una hora límite para completar la tarea? (S/N)");
+                let mut has_due_time_input = String::new();
+                io::stdin()
+                    .read_line(&mut has_due_time_input)
+                    .expect("Error al leer la entrada");
+
+                let due_time = if has_due_time_input.trim().to_lowercase() == "s" {
+                    println!("Ingresa la hora límite (por ejemplo, '14:30'): ");
+                    let mut due_time_input = String::new();
+                    io::stdin()
+                        .read_line(&mut due_time_input)
+                        .expect("Error al leer la hora límite");
+                    Some(due_time_input.trim().to_string())
+                } else {
+                    None
+                };
+
+                todo_list.add_task(task_input.trim().to_string(), due_time);
             }
-            "2" => todo_list.show_tasks(),
+            "2" => {
+                todo_list.display_tasks();
+            }
             "q" => {
                 println!("Saliendo del programa.");
                 break;
             }
-            _ => println!("Opción no válida. Inténtelo de nuevo."),
+            _ => {
+                println!("Opción no válida. Inténtelo de nuevo.");
+            }
         }
     }
 }
 
 fn print_menu() {
     println!("------ Menú ------");
-    println!("1. Agrega tarea");
-    println!("2. Mostrar tareas pendientes");
+    println!("1. Agregar tarea");
+    println!("2. Mostrar tareas");
     println!("q. Salir");
     print!("Seleccione una opción: ");
     io::stdout().flush().unwrap();
